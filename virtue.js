edf72a708d19616de5de669bab3cd6f52b4dee7e
yaashwin <muhammedyaashwin@gmail.com>
2024-03-07 09:30:53 +0530

    deleted

--- virtue.js
+++
@@ -1,8 +0,0 @@
-function checkOTP() {
-    var enteredOTP = document.getElementById('otp').value;
-    if (enteredOTP === '1122') {
-        alert('OTP is successful!');
-    } else {
-        alert('Invalid OTP. Please try again.');
-    }
-}
